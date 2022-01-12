c5f5e68c8b6cd2ae3725785d62b520e0009bb5e2
gowtham24008 <gowtham24008@gmail.com>
2022-01-12 09:58:28 +0530

    task done

---
+++ src/org/test/GreensBank.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class GreensBank  {
+
+	private void empId() {
+		System.out.println("1234567");
+	}
+      private void empName() {
+		System.out.println("shakthi");
+	}
+
+
+}
